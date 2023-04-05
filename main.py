ad3a4c7ac086fb2ec7869df13df92312c96e205b
tejasps2001 <tejasps2001@gmail.com>
2023-04-05 12:51:21 +0530

    Created main and added function to change invalid filenames

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+from tkinter import filedialog
+def main():
+    for file in os.listdir(source_folder):
+        # Rename file if it contains space.
+        if ' ' in file:
+            # print(str(file) + ' contains space!')
+            file = convert_filename(file)
+
+def convert_filename(old_filename):
+    return old_filename.replace(' ', '_')
+
+if __name__ == '__main__':
+    source_folder = filedialog.askdirectory(mustexist=True)
+    main()
