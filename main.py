0e7937dcc54f1215d8cdf28701629a0b4b4c5d8d
tejasps2001 <tejasps2001@gmail.com>
2023-04-05 13:42:33 +0530

    Fixed bug of convert_filename function and added management for video files

--- main.py
+++ main.py
@@ -4,17 +4,33 @@ import os
 import shutil
 
 from tkinter import filedialog
+
+
 def main():
     for file in os.listdir(source_folder):
         # Rename file if it contains space.
-        if ' ' in file:
+        if " " in file:
             # print(str(file) + ' contains space!')
-            file = convert_filename(file)
+            file = convert_filename(file, source_folder)
 
-def convert_filename(old_filename):
-    return old_filename.replace(' ', '_')
+        # Detect file type and move to appropriate directory.
+        if file.endswith((".mkv", ".mp4")):
+            destination = shutil.move(
+                os.path.join(source_folder, file),
+                os.path.join(home_directory, "Videos"),
+            )
+            print(destination)
 
-if __name__ == '__main__':
+
+def convert_filename(old_filename, source_folder):
+    new_filename = old_filename.replace(" ", "_")
+    return shutil.move(
+        os.path.join(source_folder, old_filename),
+        os.path.join(source_folder, new_filename),
+    )
+
+
+if __name__ == "__main__":
     source_folder = filedialog.askdirectory(mustexist=True)
-    home_directory = os.path.expanduser('~')
+    home_directory = os.path.expanduser("~")
     main()
