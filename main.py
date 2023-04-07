b7a4287c432119bddd0e5f309f7a0dceee42f68c
tejasps2001 <tejasps2001@gmail.com>
2023-04-07 15:06:11 +0530

    added logging, testing for logging and removed global status of source_folder variable

--- main.py
+++ main.py
@@ -2,11 +2,25 @@
 
 import os
 import shutil
+import logging
 
 from tkinter import filedialog
 
+home_directory = os.path.expanduser("~")
+logging.basicConfig(
+    filename=os.path.join(home_directory, "File-Transfer-Log.txt"),
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+)
 
-def main():
+
+def file_transfer(source_folder, home_directory):
+    """Transfer files from source_folder to appropriate directories.
+
+    Args:
+        source_folder (str): folder to manage selected by user.
+        home_directory (str): home directory file path.
+    """
     for file in os.listdir(source_folder):
         # Rename file if it contains space.
         if " " in file:
@@ -19,9 +33,27 @@ def main():
                 os.path.join(source_folder, file),
                 os.path.join(home_directory, "Videos"),
             )
+            destination = os.path.dirname(destination)
+            logging.info("Moved " + file + " to " + destination)
+            print(
+                "Moved \033[35m"
+                + file
+                + "\033[39m to \033[33m"
+                + destination
+                + "\033[39m"
+            )
 
 
 def convert_filename(old_filename, source_folder):
+    """Convert invalid filenames to valid filenames.
+
+    Args:
+        old_filename (str): invalid filename.
+        source_folder (str): source folder to get filenames from.
+
+    Returns:
+        str: absolute filepath to the new valid filename.
+    """
     new_filename = old_filename.replace(" ", "_")
     return shutil.move(
         os.path.join(source_folder, old_filename),
@@ -29,7 +61,10 @@ def convert_filename(old_filename, source_folder):
     )
 
 
-if __name__ == "__main__":
+def main():
     source_folder = filedialog.askdirectory(mustexist=True)
-    home_directory = os.path.expanduser("~")
+    file_transfer(source_folder, home_directory)
+
+
+if __name__ == "__main__":
     main()
--- test_main.py
+++ test_main.py
@@ -1,13 +1,45 @@
 #! /usr/bin/env python3
 
+import os
+import main
+import random
 import unittest
 
-from main import convert_filename
 
 class TestMain(unittest.TestCase):
     def test_wrong_filename(self):
-        self.assertEqual('file_1.txt', convert_filename('file 1.txt'))
-        self.assertEqual('file_abc.txt', convert_filename('file abc.txt'))
+        self.f1 = open("file 1.txt", "w")
+        self.f2 = open("file abc.txt", "w")
+        self.assertEqual(
+            "file_1.txt",
+            os.path.basename(main.convert_filename("file 1.txt", os.getcwd())),
+        )
+        self.assertEqual(
+            "file_abc.txt",
+            os.path.basename(main.convert_filename("file abc.txt", os.getcwd())),
+        )
+        self.f1.close()
+        self.f2.close()
+        os.remove(os.path.join(os.getcwd(), "file_1.txt"))
+        os.remove(os.path.join(os.getcwd(), "file_abc.txt"))
 
-if __name__ == '__main__':
+    def test_logging(self):
+        home_directory = os.path.expanduser("~")
+        # Give a random number in the name to make the name unique everytime.
+        # This will make it easy to test multiple times without changing name of file.
+        self.f3 = open(
+            "test_video" + str(random.choice(range(1000, 9999))) + ".mkv", "wb"
+        )
+        main.file_transfer(os.getcwd(), home_directory)
+        with open(os.path.join(home_directory, "File-Transfer-Log.txt"), "r") as f:
+            log_content = f.read()
+        self.assertIn(
+            "Moved " + self.f3.name + " to /home/top2001/Videos",
+            log_content,
+        )
+        self.f3.close()
+        os.remove(os.path.join(home_directory, "Videos", self.f3.name))
+
+
+if __name__ == "__main__":
     unittest.main()
