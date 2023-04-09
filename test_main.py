cf0471421471b56d53ef3ca354e646c7ea94ee1a
tejasps2001 <tejasps2001@gmail.com>
2023-04-09 16:15:32 +0530

    Added testing for file transfer

--- test_main.py
+++ test_main.py
@@ -40,6 +40,47 @@ class TestMain(unittest.TestCase):
         self.f3.close()
         os.remove(os.path.join(home_directory, "Videos", self.f3.name))
 
+    def test_transfer(self):
+        home_directory = os.path.expanduser("~")
+        # Test one file type from each category.
+        # Give a random number in the name of the file to make it unique everytime.
+        # This will make it easy to verify that the transfer worked.
+        self.video = open(
+            "test_video" + str(random.choice(range(1000, 9999))) + ".mkv", "wb"
+        )
+        self.music = open(
+            "test_music" + str(random.choice(range(1000, 9999))) + ".mp3", "wb"
+        )
+        self.picture = open(
+            "test_picture" + str(random.choice(range(1000, 9999))) + ".jpg", "wb"
+        )
+        self.doc = open(
+            "test_doc" + str(random.choice(range(1000, 9999))) + ".xlsx", "wb"
+        )
+        main.file_transfer(os.getcwd(), home_directory)
+        # Verify file transfers.
+        self.assertIn(
+            self.video.name, os.listdir(os.path.join(home_directory, "Videos"))
+        )
+        self.assertIn(
+            self.music.name, os.listdir(os.path.join(home_directory, "Music"))
+        )
+        self.assertIn(
+            self.picture.name, os.listdir(os.path.join(home_directory, "Pictures"))
+        )
+        self.assertIn(
+            self.doc.name, os.listdir(os.path.join(home_directory, "Documents"))
+        )
+        # Close file objects and delete files.
+        self.video.close()
+        self.music.close()
+        self.picture.close()
+        self.doc.close()
+        os.remove(os.path.join(home_directory, "Videos", self.video.name))
+        os.remove(os.path.join(home_directory, "Music", self.music.name))
+        os.remove(os.path.join(home_directory, "Pictures", self.picture.name))
+        os.remove(os.path.join(home_directory, "Documents", self.doc.name))
+
 
 if __name__ == "__main__":
     unittest.main()
